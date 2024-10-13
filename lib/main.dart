ac81c818dba885118239273bb42d6360c0d10c39
ducduy trinh <trinhducduy2808@gmail.com>
2024-10-13 19:31:12 +0700

    first solution: random alphabet but it's too slow

--- lib/main.dart
+++ lib/main.dart
@@ -1,125 +1,86 @@
-import 'package:flutter/material.dart';
+import 'dart:convert';
+import 'dart:math';
+import 'package:http/http.dart' as http;
+
+/// [
+///     {
+///         "slot": 0,
+///         "guess": "b",
+///         "result": "absent"
+///     },
+///     {
+///         "slot": 1,
+///         "guess": "a",
+///         "result": "present"
+///     },
+///     {
+///         "slot": 2,
+///         "guess": "r",
+///         "result": "absent"
+///     },
+///     {
+///         "slot": 3,
+///         "guess": "o",
+///         "result": "present"
+///     },
+///     {
+///         "slot": 4,
+///         "guess": "n",
+///         "result": "absent"
+///     }
+/// ]
 
 void main() {
-  runApp(const MyApp());
+  int wordSize = 6;
+  guessWord(wordSize);
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+Future<void> guessWord(int wordSize) async {
+  /// init alphabet
+  List<String> alphabet = 'abcdefghijklmnopqrstuvwxyz'.split('');
+  String result = '';
 
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
+  while (result.isEmpty) {
+    /// get random word
+    String randomWord = generateRandomWord(alphabet, wordSize);
+    print('randomWord is guessing: $randomWord');
+
+    /// call API
+    final response = await http.get(Uri.parse(
+        'https://wordle.votee.dev:8000/daily?guess=$randomWord&size=$wordSize'));
+
+    if (response.statusCode == 200) {
+      List feedback = jsonDecode(response.body);
+
+      bool isCorrect = true;
+      for (int i = 0; i < feedback.length; i++) {
+        if (feedback[i]['result'] != 'correct') {
+          isCorrect = false;
+          break;
+        }
+      }
+
+      if (isCorrect) {
+        result = randomWord;
+        print('final result : $result');
+      } else {
+        alphabet = removeWrongCharacters(alphabet, feedback);
+      }
+    } else {
+      print('error: ${response.statusCode} - ${response.body}');
+    }
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
+String generateRandomWord(List<String> alphabet, int wordSize) => List.generate(
+    wordSize, (index) => alphabet[Random().nextInt(alphabet.length)]).join();
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
+List<String> removeWrongCharacters(List<String> alphabet, List feedback) {
+  for (var feedbackChild in feedback) {
+    if (feedbackChild['result'] == 'absent' &&
+        alphabet.contains(feedbackChild['guess'])) {
+      alphabet.remove(feedbackChild['guess']);
+    }
   }
-
-  @override
-  Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
-    );
-  }
+  return alphabet;
 }
